7eaa1a2fa471d3d52b382c2d81c6f2591a565239
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2026-03-09 10:31:40 +0530

    [Pushpendra Upadhyay] Add. feature/UC10-GenericQuantity.

---
+++ QuantityMeasurementApp/Model/IMeasurable.cs
@@ -0,0 +1,10 @@
+namespace QuantityMeasurementApp.Model
+{
+    public interface IMeasurable
+    {
+        double GetConversionFactor();
+        double ConvertToBaseUnit(double value);
+        double ConvertFromBaseUnit(double baseValue);
+        string GetUnitName();
+    }
+}
